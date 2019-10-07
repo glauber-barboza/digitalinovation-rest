d10aa91241b0de7d4ad2a9900e4662830ff95d9e
glauber.barboza <glauber.barboza@dbccompany.com.br>
2019-10-07 18:29:09 -0300

    adicionar codigo de filters

--- filters/src/main/java/digitalinnovation/example/restfull/controller/SoldadoController.java
+++ filters/src/main/java/digitalinnovation/example/restfull/controller/SoldadoController.java
@@ -9,10 +9,14 @@ import digitalinnovation.example.restfull.service.SoldadoService;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/soldado")
@@ -34,6 +38,7 @@ public class SoldadoController {
 
     @PostMapping
     public ResponseEntity criarSoldado(@RequestBody Soldado soldado) {
+        System.out.println("Entrou no post");
         soldadoService.criarSoldado(soldado);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
